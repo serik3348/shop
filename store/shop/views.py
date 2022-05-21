bf2352b1d649df39ae8727eb6ea05d243f641a65
serik3348 <onbolsyn.serik@gmail.com>
2022-05-21 16:18:43 +0600

    first commit

---
+++ store/shop/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("asdasdasda")
+
+def index(request):
+    return HttpResponse("<h1>Hello my name is Serik</h1>")
+
